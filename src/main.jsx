03d9fd29787bd11f7612ef87f727d9d1a667a48b
Mridul <iammridul012@gamil.com>
2025-04-27 15:10:09 +0530

    routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
-import { HashRouter } from 'react-router-dom';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
 import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter>
     <StrictMode>
-      <App />
-      <Analytics />
-    </StrictMode>
-  </HashRouter>
-);
+    <App />
+    <Analytics />
+  </StrictMode>,
+</BrowserRouter>
+)
